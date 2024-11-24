dcc4701bd2b66f12edc5a40a5005c63d1b6ecd89
galigoldman <gali.goldman@post.runi.ac.il>
2024-11-24 12:02:11 +0200

    fix

--- Algebra.java
+++ Algebra.java
@@ -84,6 +84,13 @@ public class Algebra {
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
 		int result=x, i=0;
+		if(n==0)
+		{
+			return 1;
+		}
+		if(x==0){
+			return 0;
+		}
 		for(i=0; i<n-1; i++)
 		{
 			result=times(result, x);
@@ -95,22 +102,21 @@ public class Algebra {
 	public static int div(int x1, int x2) {
 		int num=x1, count=0;
 		boolean neg=false;
-		if(x1<0){
-			if(x2<0){
-				x1=minus(0, x1);
+		if(x1==0){
+			return 0;
+		}
+		if(x1<0 && x2<0){
+			x1=minus(0, x1);
+			x2=minus(0, x2);
+		}
+		if(x1<0 && x2>0){
+			x1=minus(0, x1);
+			neg=true;
+		}
+		if(x2<0 && x1>0){
 				x2=minus(0, x2);
-			}
-			if(x2>0){
-				x1=minus(0, x1);
 				neg=true;
 			}
-		}
-		if(x2<0){
-			if(x1>0){
-				x2=minus(0, x2);
-				neg=true;
-			}
-		}
 		while (num>0 && (num>x2 || num == x2)) {
 			num=minus(num, x2);
 			count++;
